1d162984b6d788fc26ec10782187d614206b8c0d
Allan Ojera <ojrallan@gmail.com>
2026-06-26 12:26:13 +0300

    Create order routes file

---
+++ src/routes/order.routes.js
@@ -0,0 +1,39 @@
+import express from "express";
+import * as orderController from "../controllers/order.controller.js";
+import auth from "../middleware/role.middleware.js";
+import authorize from "../middleware/role.middleware.js";
+import { createOrderValidation } from "../validators/order.validator.j";
+import { validateRequest } from "../middleware/validate.middleware.js";
+
+const router = express.Router();
+
+router.get("/", auth, orderController.getOrders);
+
+router.get("/:id", auth, orderController.getOrder);
+
+router.post(
+  "/",
+  auth,
+  authorize("Admin", "Procurement Officer"),
+  createOrderValidation,
+  validateRequest,
+  orderController.createOrder,
+);
+
+router.put(
+  "/:id",
+  auth,
+  authorize("Admin", "Procurement Officer"),
+  createOrderValidation,
+  validateRequest,
+  orderController.updateOrder,
+);
+
+router.delete(
+  "/:id",
+  auth,
+  authorize("Admin", "Procurement Officer"),
+  orderController.deleteOrder,
+);
+
+export default router;
